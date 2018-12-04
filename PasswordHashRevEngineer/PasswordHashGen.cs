ef3f71a13b8d1091bac8c45b65fa53f561b9a3d7
Oluwaremi Oluwole <polymathwole@gmail.com>
2018-12-04 21:59:35 +0100

    Init commit

---
+++ PasswordHashRevEngineer/PasswordHashGen.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PasswordHashRevEngineer
+{
+    class PasswordHashGen
+    {
+        public static string PasswordHash(string password, string staffid)
+        {
+            try
+            {
+                if (password.Trim() == string.Empty)
+                {
+                    //do not process empty passwords
+                    throw new Exception("Custom Error: Cannot process Empty password");
+                }
+                //create temp for storing and sending hash
+                byte[] tempPassword;
+                byte[] tempHash;
+
+                tempPassword = Encoding.ASCII.GetBytes(password + staffid);
+                HashAlgorithm passwordhash = new MD5CryptoServiceProvider();
+                tempHash = passwordhash.ComputeHash(tempPassword);
+
+                //getting the new hashed value
+
+                StringBuilder hashedpassword = new StringBuilder(tempHash.Length);
+                for (int i = 0; i <= tempHash.Length - 1; i++)
+                {
+                    hashedpassword.Append(tempHash[i].ToString("X2"));
+                }
+                return hashedpassword.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message + " from " + ex.TargetSite);
+            }
+        }
+
+        public string getHash(string clearTextPass, string saltText)
+        {
+
+            byte[] kb = Encoding.UTF8.GetBytes(string.Concat(clearTextPass));
+            byte[] sb = Encoding.UTF8.GetBytes(string.Concat(saltText));
+
+            byte[] hh = GenerateSaltedHash(kb, sb);
+
+            return Convert.ToBase64String(hh);
+
+        }
+
+        private byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
+        {
+            HashAlgorithm algorithm = new SHA256Managed();
+
+            byte[] plainTextWithSaltBytes =
+              new byte[plainText.Length + salt.Length];
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                plainTextWithSaltBytes[i] = plainText[i];
+            }
+            for (int i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltBytes);
+        }
+    }
+}
---
+++ PasswordHashRevEngineer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PasswordHashRevEngineer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keySalt = "FCSD";
+
+            PasswordHashGen pHG = new PasswordHashGen();
+            Console.Write("Enter string to hash: ");
+            string textToHash = Console.ReadLine();
+            Console.WriteLine(pHG.getHash(textToHash, keySalt));
+            // Console.WriteLine(PasswordHashGen.PasswordHash("OluwaremiEO","SN027053"));
+        }
+    }
+}
